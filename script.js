407013848d31e22c09f1502a24c5770f92f95de0
Heli384 <98775132+Heli384@users.noreply.github.com>
2024-12-16 08:19:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
       "https://www.champeimassage.com/behandlungen",   //12. Dez ->Geschenk
       "https://github.com/Heli384/Adventskalender/blob/main/haserl.jpg?raw=true",  //13. Dez Lieblingsmensch -> Geschenk
       "https://www.instagram.com/reel/DDPbzHLpiou/?igsh=ZnJqc3pmdjU1OHRk",  //14. Dez
-      "https://instagram.com/reel/15",  //15. Dez
-      "https://instagram.com/reel/16",  //16. Dez
+      "https://www.instagram.com/reel/DDW7jKrIx1T/?igsh=MXBudjJzanQ0aHdvcQ==",  //15. Dez
+      "https://www.instagram.com/reel/DCjfFUiSQMD/?igsh=MTE2N25iZm15NTJyNQ==",  //16. Dez
       "https://instagram.com/reel/17",
       "https://instagram.com/reel/18",
       "https://instagram.com/reel/19",
